129ef12ad7ffa6ec7d37a2c52aca6f699dba4de8
rhornsby <github@w.ktzr.net>
2014-09-28 15:56:12 -0500

    Initial commit

---
+++ WindowsFormsApplication1/Form1.Designer.cs
@@ -0,0 +1,220 @@
+﻿namespace WindowsFormsApplication1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.buttonGo = new System.Windows.Forms.Button();
+            this.textBoxClaimNum = new System.Windows.Forms.TextBox();
+            this.lblClaimNum = new System.Windows.Forms.Label();
+            this.progressBarClaims = new System.Windows.Forms.ProgressBar();
+            this.openFileDialogBilling = new System.Windows.Forms.OpenFileDialog();
+            this.openFileDialogResponse = new System.Windows.Forms.OpenFileDialog();
+            this.buttonPopBillingFileDialog = new System.Windows.Forms.Button();
+            this.buttonPopResponseFileDialog = new System.Windows.Forms.Button();
+            this.textBoxBillingFileName = new AutoEllipsis.TextBoxEllipsis();
+            this.textBoxResponseFileName = new AutoEllipsis.TextBoxEllipsis();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.fileToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.exitToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.lblBillingFile = new System.Windows.Forms.Label();
+            this.lblResponseFile = new System.Windows.Forms.Label();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // buttonGo
+            // 
+            this.buttonGo.Location = new System.Drawing.Point(390, 99);
+            this.buttonGo.Name = "buttonGo";
+            this.buttonGo.Size = new System.Drawing.Size(75, 23);
+            this.buttonGo.TabIndex = 0;
+            this.buttonGo.Text = "Go";
+            this.buttonGo.UseVisualStyleBackColor = true;
+            this.buttonGo.Click += new System.EventHandler(this.btnGo_Click);
+            // 
+            // textBoxClaimNum
+            // 
+            this.textBoxClaimNum.Enabled = false;
+            this.textBoxClaimNum.Location = new System.Drawing.Point(82, 161);
+            this.textBoxClaimNum.Name = "textBoxClaimNum";
+            this.textBoxClaimNum.Size = new System.Drawing.Size(172, 20);
+            this.textBoxClaimNum.TabIndex = 1;
+            // 
+            // lblClaimNum
+            // 
+            this.lblClaimNum.AutoSize = true;
+            this.lblClaimNum.Location = new System.Drawing.Point(34, 164);
+            this.lblClaimNum.Name = "lblClaimNum";
+            this.lblClaimNum.Size = new System.Drawing.Size(42, 13);
+            this.lblClaimNum.TabIndex = 2;
+            this.lblClaimNum.Text = "Claim #";
+            // 
+            // progressBarClaims
+            // 
+            this.progressBarClaims.Location = new System.Drawing.Point(37, 197);
+            this.progressBarClaims.Name = "progressBarClaims";
+            this.progressBarClaims.Size = new System.Drawing.Size(428, 23);
+            this.progressBarClaims.TabIndex = 3;
+            // 
+            // openFileDialogBilling
+            // 
+            this.openFileDialogBilling.Filter = "Excel 1997-2003|*.xls|All files|*.*";
+            this.openFileDialogBilling.FileOk += new System.ComponentModel.CancelEventHandler(this.openFileDialog1_FileOk);
+            // 
+            // openFileDialogResponse
+            // 
+            this.openFileDialogResponse.Filter = "Excel 1997-2003|*.xls|All files|*.*";
+            // 
+            // buttonPopBillingFileDialog
+            // 
+            this.buttonPopBillingFileDialog.Location = new System.Drawing.Point(434, 41);
+            this.buttonPopBillingFileDialog.Name = "buttonPopBillingFileDialog";
+            this.buttonPopBillingFileDialog.Size = new System.Drawing.Size(31, 23);
+            this.buttonPopBillingFileDialog.TabIndex = 4;
+            this.buttonPopBillingFileDialog.Text = "...";
+            this.buttonPopBillingFileDialog.UseVisualStyleBackColor = true;
+            this.buttonPopBillingFileDialog.Click += new System.EventHandler(this.buttonPopBillingFileDialog_Click);
+            // 
+            // buttonPopResponseFileDialog
+            // 
+            this.buttonPopResponseFileDialog.Location = new System.Drawing.Point(434, 71);
+            this.buttonPopResponseFileDialog.Name = "buttonPopResponseFileDialog";
+            this.buttonPopResponseFileDialog.Size = new System.Drawing.Size(31, 23);
+            this.buttonPopResponseFileDialog.TabIndex = 5;
+            this.buttonPopResponseFileDialog.Text = "...";
+            this.buttonPopResponseFileDialog.UseVisualStyleBackColor = true;
+            this.buttonPopResponseFileDialog.Click += new System.EventHandler(this.buttonPopResponseFileDialog_Click);
+            // 
+            // textBoxBillingFileName
+            // 
+            this.textBoxBillingFileName.Location = new System.Drawing.Point(138, 44);
+            this.textBoxBillingFileName.Name = "textBoxBillingFileName";
+            this.textBoxBillingFileName.Size = new System.Drawing.Size(289, 20);
+            this.textBoxBillingFileName.TabIndex = 6;
+            this.textBoxBillingFileName.MouseClick += new System.Windows.Forms.MouseEventHandler(this.textBoxBillingFileName_MouseClick);
+            this.textBoxBillingFileName.AutoEllipsis = ((AutoEllipsis.EllipsisFormat)((AutoEllipsis.EllipsisFormat.Start | AutoEllipsis.EllipsisFormat.Path)));
+            
+            // 
+            // textBoxResponseFileName
+            // 
+            this.textBoxResponseFileName.Location = new System.Drawing.Point(138, 73);
+            this.textBoxResponseFileName.Name = "textBoxResponseFileName";
+            this.textBoxResponseFileName.Size = new System.Drawing.Size(289, 20);
+            this.textBoxResponseFileName.TabIndex = 7;
+            this.textBoxResponseFileName.MouseClick += new System.Windows.Forms.MouseEventHandler(this.textBoxResponseFileName_MouseClick);
+            this.textBoxResponseFileName.AutoEllipsis = ((AutoEllipsis.EllipsisFormat)((AutoEllipsis.EllipsisFormat.Start | AutoEllipsis.EllipsisFormat.Path)));
+
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.fileToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(500, 24);
+            this.menuStrip1.TabIndex = 8;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // fileToolStripMenuItem
+            // 
+            this.fileToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.exitToolStripMenuItem});
+            this.fileToolStripMenuItem.Name = "fileToolStripMenuItem";
+            this.fileToolStripMenuItem.Size = new System.Drawing.Size(37, 20);
+            this.fileToolStripMenuItem.Text = "File";
+            // 
+            // exitToolStripMenuItem
+            // 
+            this.exitToolStripMenuItem.Name = "exitToolStripMenuItem";
+            this.exitToolStripMenuItem.Size = new System.Drawing.Size(92, 22);
+            this.exitToolStripMenuItem.Text = "Exit";
+            this.exitToolStripMenuItem.Click += new System.EventHandler(this.exitToolStripMenuItem_Click);
+            // 
+            // lblBillingFile
+            // 
+            this.lblBillingFile.AutoSize = true;
+            this.lblBillingFile.Location = new System.Drawing.Point(37, 51);
+            this.lblBillingFile.Name = "lblBillingFile";
+            this.lblBillingFile.Size = new System.Drawing.Size(50, 13);
+            this.lblBillingFile.TabIndex = 9;
+            this.lblBillingFile.Text = "Billing file";
+            // 
+            // lblResponseFile
+            // 
+            this.lblResponseFile.AutoSize = true;
+            this.lblResponseFile.Location = new System.Drawing.Point(37, 76);
+            this.lblResponseFile.Name = "lblResponseFile";
+            this.lblResponseFile.Size = new System.Drawing.Size(71, 13);
+            this.lblResponseFile.TabIndex = 10;
+            this.lblResponseFile.Text = "Response file";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(500, 312);
+            this.Controls.Add(this.lblResponseFile);
+            this.Controls.Add(this.lblBillingFile);
+            this.Controls.Add(this.textBoxResponseFileName);
+            this.Controls.Add(this.textBoxBillingFileName);
+            this.Controls.Add(this.buttonPopResponseFileDialog);
+            this.Controls.Add(this.buttonPopBillingFileDialog);
+            this.Controls.Add(this.progressBarClaims);
+            this.Controls.Add(this.lblClaimNum);
+            this.Controls.Add(this.textBoxClaimNum);
+            this.Controls.Add(this.buttonGo);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "Form1";
+            this.Text = "RMA Transcription Machine";
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button buttonGo;
+        private System.Windows.Forms.TextBox textBoxClaimNum;
+        private System.Windows.Forms.Label lblClaimNum;
+        private System.Windows.Forms.ProgressBar progressBarClaims;
+        private System.Windows.Forms.OpenFileDialog openFileDialogBilling;
+        private System.Windows.Forms.OpenFileDialog openFileDialogResponse;
+        private System.Windows.Forms.Button buttonPopBillingFileDialog;
+        private System.Windows.Forms.Button buttonPopResponseFileDialog;
+        private AutoEllipsis.TextBoxEllipsis textBoxBillingFileName;
+        private AutoEllipsis.TextBoxEllipsis textBoxResponseFileName;
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem fileToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem exitToolStripMenuItem;
+        private System.Windows.Forms.Label lblBillingFile;
+        private System.Windows.Forms.Label lblResponseFile;
+    }
+}
+
---
+++ WindowsFormsApplication1/Form1.cs
@@ -0,0 +1,234 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Text.RegularExpressions;
+using File = System.IO.File;
+using Excel = Microsoft.Office.Interop.Excel;
+using System.Runtime.InteropServices;
+using AutoEllipsis;
+
+namespace WindowsFormsApplication1
+{
+    public partial class Form1 : Form
+    {
+
+        [DllImport("shlwapi.dll", CharSet = CharSet.Auto)]
+        static extern bool PathCompactPathEx([Out] StringBuilder pszOut, string szPath, int cchMax, int dwFlags);
+
+        private Excel.Application ExcelObj = null;
+        private String billingFileName = "";
+        private String responseFileName = "";
+        private const int RESPONSE_ROW_OFFSET = 15;
+        EllipsisFormat fmt = EllipsisFormat.None;
+
+        public Form1()
+        {
+            InitializeComponent();
+            ExcelObj = new Excel.Application();
+            fmt |= EllipsisFormat.Start;
+            fmt |= EllipsisFormat.Path;
+        }
+
+        private void btnGo_Click(object sender, EventArgs e)
+        {
+            Excel.Workbook billingWb = null;
+            Excel.Workbook responseWb = null;
+            lblBillingFile.ForeColor = SystemColors.ControlText;
+            lblResponseFile.ForeColor = SystemColors.ControlText;
+
+            if (billingFileName.Length == 0 || responseFileName.Length == 0)
+            {
+                if (billingFileName.Length == 0)
+                {
+                    lblBillingFile.ForeColor = Color.Red;
+                }
+                if (responseFileName.Length == 0)
+                {
+                    lblResponseFile.ForeColor = Color.Red;
+                }
+                return;
+            }
+
+            try
+            {
+                billingWb = ExcelObj.Workbooks.Open(billingFileName, Type.Missing, System.IO.FileAccess.Read);
+            }
+            catch (System.IO.FileNotFoundException ex)
+            {
+                MessageBox.Show(ex.Message, "File not found", MessageBoxButtons.OK, MessageBoxIcon.Stop);
+                return;
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(
+                   ex.Message, "Couldn't open file", MessageBoxButtons.OK, MessageBoxIcon.Stop);
+                return;
+            }
+
+
+            try
+            {
+                responseWb = ExcelObj.Workbooks.Open(responseFileName);
+            }
+            catch (System.IO.FileNotFoundException ex)
+            {
+                MessageBox.Show(ex.Message, "File Not Found", MessageBoxButtons.OK, MessageBoxIcon.Stop);
+                return;
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(
+                    ex.Message, "Couldn't open file", MessageBoxButtons.OK, MessageBoxIcon.Stop);
+                return;
+            }
+
+            Excel.Worksheet billingWs = billingWb.Worksheets[1];
+            Excel.Worksheet responseWs = responseWb.Worksheets[1];
+
+            Excel.Range billingUsedRange = billingWs.UsedRange;
+
+            int totalRows = billingUsedRange.Rows.Count;
+
+            foreach (Excel.Range billingRow in billingUsedRange.Rows) {
+                // skip first row headers
+                if (billingRow.Row == 1)
+                    continue;
+
+                Decimal progress = (Decimal)billingRow.Row / (Decimal)totalRows;
+                progressBarClaims.Value = (int)Math.Round(progress * 100, 0);
+
+                Excel.Range cell = (Excel.Range)billingRow.Columns["A"];
+                textBoxClaimNum.Text = cell.Value2.ToString();
+
+                // Get the responseRow - the row where we'll be copying TO
+                int responseRowIdx = billingRow.Row + RESPONSE_ROW_OFFSET;
+                Excel.Range responseRow = null;
+                if ((Excel.Range)responseWs.Rows[responseRowIdx] == null)
+                {
+                    responseRow = (Excel.Range)responseWs.Rows[responseRowIdx];
+                    responseRow.Insert();
+                }
+                else
+                {
+                    responseRow = (Excel.Range)responseWs.Rows[responseRowIdx];
+                }
+
+                copyColumns(billingRow, responseRow);
+
+            }
+
+            responseWb.Save();
+            responseWb.Close();
+            billingWb.Close();
+
+            lblClaimNum.Text = "";
+            System.Diagnostics.Debug.WriteLine("done!");
+        }
+
+        private void copyColumns(Excel.Range billingRow, Excel.Range responseRow)
+        {
+            this.copy(billingRow, "A", responseRow, "A");
+            this.copy(billingRow, "I", responseRow, "D");
+            this.copy(billingRow, "J", responseRow, "E");
+            this.copy(billingRow, "M", responseRow, "H");
+            this.copy(billingRow, "M", responseRow, "I");
+            this.copy(billingRow, "O", responseRow, "J");
+            this.copy(billingRow, "O", responseRow, "K");
+            this.copy(billingRow, "Q", responseRow, "L");
+            this.copy(billingRow, "Q", responseRow, "M");
+            this.copy(billingRow, "S", responseRow, "N");
+            this.copy(billingRow, "S", responseRow, "O");
+            this.copy(billingRow, "AH", responseRow, "AD");
+            this.copy(billingRow, "F", responseRow, "AE");
+            this.copy(billingRow, "AJ", responseRow, "AK");
+            this.copy(billingRow, "AR", responseRow, "AL");
+            this.copy(billingRow, "AZ", responseRow, "AM");
+            this.copy(billingRow, "BH", responseRow, "AN");
+            this.copy(billingRow, "BP", responseRow, "AO");
+            this.copy(billingRow, "BX", responseRow, "AP");
+            this.copy(billingRow, "CF", responseRow, "AQ");
+            this.copy(billingRow, "CX", responseRow, "AT");
+            this.copy(billingRow, "X", responseRow, "AU");
+        }
+
+        private void copy(Excel.Range sourceRow, string sourceColumn, Excel.Range targetRow, string targetColumn)
+        {
+            Excel.Range cell = (Excel.Range)sourceRow.Columns[sourceColumn].Cells;
+            if (cell.Value2 != null)
+            {
+                targetRow.Columns[targetColumn].Cells.Value2 = sourceRow.Columns[sourceColumn].Cells.Value2;
+            }
+        }
+
+        private void openFileDialog1_FileOk(object sender, CancelEventArgs e)
+        {
+
+        }
+
+        private void buttonPopBillingFileDialog_Click(object sender, EventArgs e)
+        {
+            lblBillingFile.ForeColor = SystemColors.ControlText;
+            DialogResult result = openFileDialogBilling.ShowDialog();
+            if (result == DialogResult.OK)
+            {
+                textBoxBillingFileName.Text = this.billingFileName = openFileDialogBilling.FileName;
+            }
+
+        }
+
+        private void buttonPopResponseFileDialog_Click(object sender, EventArgs e)
+        {
+            lblResponseFile.ForeColor = SystemColors.ControlText;
+            DialogResult result = openFileDialogResponse.ShowDialog();
+            if (result == DialogResult.OK)
+            {
+                textBoxResponseFileName.Text = this.responseFileName = openFileDialogResponse.FileName;
+            }
+        }
+
+
+        private void textBoxBillingFileName_MouseClick(object sender, MouseEventArgs e)
+        {
+            //buttonPopBillingFileDialog.PerformClick();
+        }
+        private void textBoxResponseFileName_MouseClick(object sender, MouseEventArgs e)
+        {
+            //buttonPopResponseFileDialog.PerformClick();
+        }
+
+        private void exitToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+
+        private void bw_DoWork(object sender, DoWorkEventArgs e)
+        {
+            BackgroundWorker worker = sender as BackgroundWorker;
+
+            for (int i = 1; (i <= 10); i++)
+            {
+                if ((worker.CancellationPending == true))
+                {
+                    e.Cancel = true;
+                    break;
+                }
+                else
+                {
+                    // Perform a time consuming operation and report progress.
+                    System.Threading.Thread.Sleep(500);
+                    worker.ReportProgress((i * 10));
+                }
+            }
+        }
+
+
+
+    }
+}
